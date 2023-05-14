3ba421025c68ff7aa921dec3cd73ae5868cd7116
LAB1 ADS <asek05052005@gmail.com>
2023-05-15 02:25:29 +0600

    :/

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyHashtable<MyTestingClass, Student> table = new MyHashtable<>();
+
+        Random random = new Random();
+
+        // Add 10,000 random elements to the hash table
+        for (int i = 0; i < 10000; i++) {
+            int id = random.nextInt(10000); // Random id between 0 and 9999
+            String name = "Name" + i;
+            MyTestingClass key = new MyTestingClass(id, name);
+            Student value = new Student(id, "Student" + i);
+            table.put(key, value);
+        }
+
     }
 }
