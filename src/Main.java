715a450ab9e5a395bb792e833f0cb295fbf2ec65
LAB1 ADS <asek05052005@gmail.com>
2023-05-15 10:27:09 +0600

    :/

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ public class Main {
 
         Random random = new Random();
 
-        // Add 10,000 random elements to the hash table
+
         for (int i = 0; i < 10000; i++) {
-            int id = random.nextInt(10000); // Random id between 0 and 9999
+            int id = random.nextInt(10000);
             String name = "Name" + i;
             MyTestingClass key = new MyTestingClass(id, name);
             Student value = new Student(id, "Student" + i);
